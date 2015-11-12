e0985fdc1f123975f65319b8f81d22de8a83f40b
telucis <ustbwy@126.com>
2015-11-12 23:35:13 +0800

    update task0002_1
    add function trim and unique.

---
+++ task0002/js/task0002_1.js
@@ -0,0 +1,40 @@
+window.onload=function(){
+	var oInput=document.getElementsByTagName("input")[0];
+	var oBtn=document.getElementsByTagName("input")[1];
+	var oP=document.getElementsByTagName("p")[0];
+	var oArr=[];
+	function trim(string){
+		return string.replace(/(^\s*)|(\s*$)/g,'');
+	}
+	function unique(arr){
+		var result=[] , isrepeat;
+		for(var i=0;i<arr.length;i++){
+			isrepeat=false;
+			for(var j=0;j<result.length;j++){
+				if(arr[i]==result[j]){
+					isrepeat=true;
+					break;
+				}
+			}
+			if(!isrepeat){
+				result.push(arr[i])	
+			}
+		}
+		return result;
+	}
+	oBtn.onclick=function(){
+		//console.log(oInput.value.split(","));
+		oArr=[]
+		var oBrr=oInput.value.split(',');
+		var oCrr=[];
+		for(var i=0;i<oBrr.length;i++){
+			oBrr[i]=trim(oBrr[i]);
+			if(oBrr[i]!=''){
+				oCrr.push(oBrr[i]);
+			}
+		}
+		oArr=unique(oCrr);
+		oP.innerHTML=oArr;
+	}
+
+}
