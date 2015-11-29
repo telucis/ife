9bff0f32ebbd98fa005ede9930ae631eba11c089
telucis <ustbwy@126.com>
2015-11-29 22:27:29 +0800

    update_task0003_20151129

--- task0002/js/task0003_style.js
+++
@@ -1,8 +0,0 @@
-function subStyle(){
-	var manage=document.getElementsByClassName('manage')[0];
-	var main=document.getElementsByClassName('main')[0];
-	var detail=document.getElementsByClassName('detail')[0];
-	manage.style.height=document.body.offsetHeight-60-60+'px';
-	main.style.height=document.body.offsetHeight-60+'px';
-	detail.style.width=document.body.offsetWidth-400+'px';
-}
